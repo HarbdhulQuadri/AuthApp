d0fd217c7ae9a7f3f676c19636afd60fdd434f67
HarbdhulQuadri <adegbijilanre@gmail.com>
2021-02-22 00:56:38 +0100

    commited

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,10 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/register', function(req, res, next) {
-  res.render('register');
+  res.render('register',{title:'Register'});
+});
+router.get('/login', function(req, res, next) {
+  res.render('login',{title:"Login"});
 });
 
 module.exports = router;
