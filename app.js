d1b361608dc38a825356c3e49fddebb7cf259102
HarbdhulQuadri <adegbijilanre@gmail.com>
2021-02-21 22:00:30 +0100

    omo i made an error committing earlier

--- app.js
+++ app.js
@@ -1,65 +1,49 @@
-var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-var logger = require('morgan');
-session = require('express-session');
-passport =require("passport");
-expressValidator = require('express-validator');
-localStrategy = require(passport-local).Strategy;
-multer = require("multer");
-var upload = multer({dest :"./uploads"})
-flash= require("connect-flash");
-var mongoose = require(mongoose);
-//connecting to cloud mongoose
-db = mongoose.connect('mongodb+srv://admin:ishola96@cluster0.pi2rw.mongodb.net/test?retryWrites=true&w=majority')
-.then(() => {
-  console.log('Successfully connected to MongoDB Atlas!');
-})
-.catch((error) => {
-  console.log('Unable to connect to MongoDB Atlas!');
-  console.error(error);
-});
+var session = require('express-session');
+var passport = require('passport');
+const { body } = require('express-validator');
+var LocalStrategy = require('passport-local').Strategy;
+var multer = require('multer');
+var upload = multer({dest: './uploads'});
+var flash = require('connect-flash');
+var mongo = require('mongodb');
+var mongoose = require('mongoose');
+var db = mongoose.connection;
 
-
-
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-const { session } = require('passport');
-const passport = require('passport');
-const multer = require('multer');
+var routes = require('./routes/index');
+var users = require('./routes/users');
 
 var app = express();
-// handling file uploads
-app.use(multer({dest :"./uploads"}));
-//handle session 
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Handle Sessions
 app.use(session({
   secret:'secret',
-  saveUnitialized:true,
-  resave: true,
+  saveUninitialized: true,
+  resave: true
 }));
-//passport authentication 
+
+// Passport
 app.use(passport.initialize());
 app.use(passport.session());
 
-app.use(expressValidator({
-  errorFormatter: function(param, msg, value) {
-      var namespace = param.split('.')
-      , root    = namespace.shift()
-      , formParam = root;
 
-    while(namespace.length) {
-      formParam += '[' + namespace.shift() + ']';
-    }
-    return {
-      param : formParam,
-      msg   : msg,
-      value : value
-    };
-  }
-}));
-//connect flash 
 app.use(require('connect-flash')());
 app.use(function (req, res, next) {
   res.locals.messages = require('express-messages')(req, res);
@@ -76,34 +60,29 @@ app.use(function(req, res, next) {
   next(err);
 });
 
+// error handlers
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: err
+    });
+  });
+}
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
-
-// error handler
+// production error handler
+// no stacktraces leaked to user
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render('error', {
+    message: err.message,
+    error: {}
+  });
 });
+
 
 module.exports = app;
