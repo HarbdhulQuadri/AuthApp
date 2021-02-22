0ce012fc6b20b35450b2729e84382a759794cd3a
HarbdhulQuadri <adegbijilanre@gmail.com>
2021-02-22 14:07:36 +0100

    ok

--- routes/users.js
+++ routes/users.js
@@ -8,5 +8,13 @@ router.get('/register', function(req, res, next) {
 router.get('/login', function(req, res, next) {
   res.render('login',{title:"Login"});
 });
+router.post('/register',upload.single('profileImage'),function(req, res, next) {
+  var name = req.body.name;
+  var email = req.body.email;
+  var username = req.body.username;
+  var password = req.body.password;
+  var password2 = req.body.password2;
+
+});
 
 module.exports = router;
