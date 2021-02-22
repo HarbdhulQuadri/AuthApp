382c5967caffc768baab649d98f8cb8326645dd5
HarbdhulQuadri <adegbijilanre@gmail.com>
2021-02-22 14:50:44 +0100

    im moving

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
-
+var multer = require('multer');
+var upload = multer({dest: './uploads'});
 /* GET users listing. */
 router.get('/register', function(req, res, next) {
   res.render('register',{title:'Register'});
@@ -8,13 +9,29 @@ router.get('/register', function(req, res, next) {
 router.get('/login', function(req, res, next) {
   res.render('login',{title:"Login"});
 });
-router.post('/register',upload.single('profileImage'),function(req, res, next) {
+router.post('/register',upload.single('profileimage'),function(req, res, next) {
   var name = req.body.name;
   var email = req.body.email;
   var username = req.body.username;
   var password = req.body.password;
   var password2 = req.body.password2;
+if (req.file){
+ console.log('File Uploading ');
+ var profileimage= req.file.filename;
+}else{
+  console.log('no file uploaded');
+  var profileimage = 'noimage.jpg'
+}
+//Validating Form 
+req.checkBody('name','name is required').notEmpty;
 
+//Check Errors
+var errors = req.validationErrors();
+if (errors){
+  console.log('Errors');
+}else{
+  console.log("No Errors");
+}
 });
 
 module.exports = router;
